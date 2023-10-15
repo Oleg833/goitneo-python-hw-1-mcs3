a489742a5f27b3b0473000ef5bb45d98eec0caa3
Oleg833 <oleg.kovtun447@gmail.com>
2023-10-15 06:47:08 +0300

    add import

---
+++ assistant.py
@@ -0,0 +1,49 @@
+def parse_input(user_input):
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, *args
+
+
+def validate_args(args):
+    if len(args) != 2 or not args[1].isdigit():
+        return False
+    return True
+
+
+def add_contact(args, contacts):
+    if not validate_args(args):
+        return "Invalid args."
+    name, phone = args
+    contacts[name] = phone
+    return "Contact added."
+
+
+def change_contact(args, contacts):
+    if not validate_args(args):
+        return "Invalid number phone."
+    name, phone = args
+    if not name in contacts.keys():
+        return f"Name: {name} not found!"
+    contacts[name] = phone
+    return "Contact updated."
+
+
+def show_phone(args, contacts):
+    if len(args) != 1:
+        return "Invalid name."
+    name = args[0]
+    if not name in contacts:
+        return f"Name: {name} not found!"
+    return contacts[name]
+
+
+def show_all(contacts):
+    formatted_list = []
+    for name, phone in contacts.items():
+        formatted_list.append(f"{name}: {phone}")
+
+    return formatted_list
+
+
+if __name__ == "__main__":
+    print("Welcome to the assistant function!")
--- main.py
+++ main.py
@@ -1,48 +1,4 @@
-def parse_input(user_input):
-    cmd, *args = user_input.split()
-    cmd = cmd.strip().lower()
-    return cmd, *args
-
-
-def validate_args(args):
-    if len(args) != 2 or not args[1].isdigit():
-        return False
-    return True
-
-
-def add_contact(args, contacts):
-    if not validate_args(args):
-        return "Invalid args."
-    name, phone = args
-    contacts[name] = phone
-    return "Contact added."
-
-
-def change_contact(args, contacts):
-    if not validate_args(args):
-        return "Invalid number phone."
-    name, phone = args
-    if not name in contacts.keys():
-        return f"Name: {name} not found!"
-    contacts[name] = phone
-    return "Contact updated."
-
-
-def show_phone(args, contacts):
-    if len(args) != 1:
-        return "Invalid name."
-    name = args[0]
-    if not name in contacts:
-        return f"Name: {name} not found!"
-    return contacts[name]
-
-
-def show_all(contacts):
-    formatted_list = []
-    for name, phone in contacts.items():
-        formatted_list.append(f"{name}: {phone}")
-
-    return formatted_list
+import assistant
 
 
 def main():
@@ -50,20 +6,20 @@ def main():
     print("Welcome to the assistant bot!")
     while True:
         user_input = input("Enter a command: ")
-        command, *args = parse_input(user_input)
+        command, *args = assistant.parse_input(user_input)
         if command in ["close", "exit", "bye"]:
             print("Good bye!")
             break
         elif command == "hello":
             print("How can I help you?")
         elif command == "add":
-            print(add_contact(args, contacts))
+            print(assistant.add_contact(args, contacts))
         elif command == "change":
-            print(change_contact(args, contacts))
+            print(assistant.change_contact(args, contacts))
         elif command == "phone":
-            print(show_phone(args, contacts))
+            print(assistant.show_phone(args, contacts))
         elif command == "all":
-            print(show_all(contacts))
+            print(assistant.show_all(contacts))
         else:
             print("Invalid command.")
 
